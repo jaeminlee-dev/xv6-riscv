bb2da7b6a85e7ed1e4be0f659f0b41ceb02a8a75
Jaemin Lee <ljm005302@gmail.com>
2025-12-16 17:51:12 +0900

    FIx : writei 호출 시 strlen(target) + 1 바이트를 기록하도록 수정

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -320,7 +320,7 @@ sys_symlink(void)
     return -1;
   }
   
-  if(writei(ip, 0, (uint64)target, 0, strlen(target)) != strlen(target)){
+  if(writei(ip, 0, (uint64)target, 0, strlen(target) + 1) != strlen(target) + 1){
     iunlockput(ip);
     end_op();
     return -1;
